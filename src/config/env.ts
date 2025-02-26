a7e0dc5a3831b9f64a2a11fd4a1b5ac8befd7d4d
englishra_ <englishra.dev@icloud.com>
2025-02-26 22:35:37 +0000

    Removing unneeded env variables.

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,21 +4,5 @@ dotenv.config();
 
 export default {
     nodeEnv: process.env.NODE_ENV,
-    encryptionKey: process.env.ENCRYPTION_KEY,
-    monzo: {
-        clientId: process.env.MONZO_CLIENT_ID,
-        clientSecret: process.env.MONZO_CLIENT_SECRET,
-        accessToken: process.env.MONZO_ACCESS_TOKEN || "",
-        accounts: {
-            personal: {
-                id: process.env.MONZO_ACCOUNTS_PERSONAL_ID
-            },
-            joint: {
-                id: process.env.MONZO_ACCOUNTS_JOINT_ID,
-                pots: {
-                    rent: process.env.MONZO_ACCOUNTS_JOINT_POTS_RENT
-                }
-            }
-        }
-    }
+    encryptionKey: process.env.ENCRYPTION_KEY
 };
